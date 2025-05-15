ed0b8ce44f9d6295242957f181210c0d6fefb8e0
Tonmay Tamuli <tonmaytamuli2000@gmail.com>
2025-05-15 10:02:54 +0000

    commit

--- macros/get_scd1_metadata.sql
+++ macros/get_scd1_metadata.sql
@@ -1,5 +1,30 @@
--- models/metadata/meta_status_new_scd1.sql
+{% macro get_scd1_metadata(model_name) %}
+  {% set query %}
+    select source_table, unique_key, order_by, columns
+    from {{ source('raw', 'meta_table') }}
+    where trim(lower(model_name)) = '{{ model_name | lower }}'
+  {% endset %}
 
-select *
-from {{ source('raw', 'meta_table') }}
-where model_name = 'status_new_scd1'
+  {% do log("SCD1 Metadata Query: " ~ query, info=True) %}
+
+  {% set results = run_query(query) %}
+
+  {% if execute and results and results.columns | length > 0 %}
+    {% set row = results.rows[0] %}
+    {% do log("SCD1 Metadata Result: " ~ row, info=True) %}
+    {% set source_table = row[0] %}
+    {% set unique_key = row[1] %}
+    {% set order_by = row[2] %}
+    {% set columns = row[3].split(',') | map('trim') | list %}
+
+    {{ return({
+      "source_table": source_table,
+      "unique_key": unique_key,
+      "order_by": order_by,
+      "columns": columns
+    }) }}
+  {% else %}
+    {% do log("No metadata found for model: " ~ model_name, info=True) %}
+    {% do exceptions.raise_compiler_error("No metadata found for model: " ~ model_name) %}
+  {% endif %}
+{% endmacro %}
--- models/example/status_new_scd1.sql
+++ models/example/status_new_scd1.sql
@@ -1,19 +1,11 @@
 -- models/example/status_new_scd1.sql
 
-{% set meta_data = load_result('meta_config').table[0] %}
-{% set model_name = 'status_new_scd1' %}
-{% set source_table = meta_data['source_table'] %}
-{% set unique_key = meta_data['unique_key'] %}
-{% set order_by = meta_data['order_by'] %}
-{% set columns = meta_data['columns'].split(',') %}
-
--- Ensure metadata dependency
-{{ ref('meta_config') }}
+{% set meta = get_scd1_metadata('status_new_scd1') %}
 
 {{ scd1_merge(
-    model_name = model_name,
-    source_table = source_table,
-    unique_key = unique_key,
-    order_by = order_by,
-    columns = columns
+    model_name = 'status_new_scd1',
+    source_table = meta.source_table,
+    unique_key = meta.unique_key,
+    order_by = meta.order_by,
+    columns = meta.columns
 ) }}
